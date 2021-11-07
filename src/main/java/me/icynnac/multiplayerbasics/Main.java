51e215092251174c04b8334977ae9df96f226416
icynnac <68450404+icynnac@users.noreply.github.com>
2021-11-07 09:57:29 -0500

    the permission messages now work for commands

--- src/main/java/me/icynnac/multiplayerbasics/Main.java
+++ src/main/java/me/icynnac/multiplayerbasics/Main.java
@@ -1,64 +1,57 @@
-package me.icynnac.bruhcmd;
+package me.icynnac.multiplayerbasics;
 
-import me.icynnac.bruhcmd.commands.*;
-import me.icynnac.bruhcmd.events.*;
-import me.icynnac.bruhcmd.guis.BruhCMDUI;
-import me.icynnac.bruhcmd.guis.ConfigUI;
-import org.bukkit.Bukkit;
+import me.icynnac.multiplayerbasics.Events.adminactEvent;
+import me.icynnac.multiplayerbasics.Events.mpbEvent;
+import me.icynnac.multiplayerbasics.Events.mpbiEvent;
+import me.icynnac.multiplayerbasics.GUIs.mpbUI;
+import me.icynnac.multiplayerbasics.GUIs.mpbiUI;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
-import java.util.logging.Logger;
 
 public final class Main extends JavaPlugin {
 
     public static Plugin instance;
+
     public static String prefix;
-    public static String notenable;
-    public static String consoleno;
+    public static String noplayer;
     public static String noperm;
+    public static String noconsole;
+
+    public static String kicksd1;
+    public static String kicksd2;
+    public static String kicksd3;
+    public static String kicksd4;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         instance = this;
 
-        saveDefaultConfig();
         prefix = Objects.requireNonNull(getConfig().getString("messages.prefix")).replaceAll("&", "§");
-        notenable = Objects.requireNonNull(getConfig().getString("messages.not-enabled")).replaceAll("&", "§");
-        consoleno = Objects.requireNonNull(getConfig().getString("messages.console-cannot-use")).replaceAll("&", "§");
-        noperm = Objects.requireNonNull(getConfig().getString("messages.no-permission-message")).replaceAll("&", "§");
+        noplayer = Objects.requireNonNull(getConfig().getString("messages.player-not-found")).replaceAll("&", "§");
+        noperm = Objects.requireNonNull(getConfig().getString("messages.no-permission")).replaceAll("&", "§");
+        noconsole = Objects.requireNonNull(getConfig().getString("messages.console-denied")).replaceAll("&", "§");
 
-        getServer().getPluginManager().registerEvents(new BruhCMDEvents(), this);
-        getServer().getPluginManager().registerEvents(new CommandsEvents(), this);
-        getServer().getPluginManager().registerEvents(new CyberbunkEvents(), this);
-        getServer().getPluginManager().registerEvents(new UpdateEvents(), this);
-        getServer().getPluginManager().registerEvents(new ConfigEvents(), this);
+        kicksd1 = Objects.requireNonNull(getConfig().getString("messages.kick-shutdown-line-1")).replaceAll("&", "§");
+        kicksd2 = Objects.requireNonNull(getConfig().getString("messages.kick-shutdown-line-2")).replaceAll("&", "§");
+        kicksd3 = Objects.requireNonNull(getConfig().getString("messages.kick-shutdown-line-3")).replaceAll("&", "§");
+        kicksd4 = Objects.requireNonNull(getConfig().getString("messages.kick-shutdown-line-4")).replaceAll("&", "§");
 
-        Objects.requireNonNull(getCommand("bruh")).setExecutor(new BruhCommands());
-        Objects.requireNonNull(getCommand("bruh reload")).setExecutor(new BruhCommands());
-        Objects.requireNonNull(getCommand("sus")).setExecutor(new SusCommand());
-        Objects.requireNonNull(getCommand("cutg")).setExecutor(new CutG());
-        Objects.requireNonNull(getCommand("yeet")).setExecutor(new YEET());
-        Objects.requireNonNull(getCommand("pop")).setExecutor(new pop());
-        Objects.requireNonNull(getCommand("bruhcmd")).setExecutor(new BruhCMDUI());
-        Objects.requireNonNull(getCommand("joe")).setExecutor(new joe());
-        Objects.requireNonNull(getCommand("cyberpunk")).setExecutor(new cyberbunk());
-        Objects.requireNonNull(getCommand("bomb")).setExecutor(new bomb());
-        Objects.requireNonNull(getCommand("creeper")).setExecutor(new creeper());
-        Objects.requireNonNull(getCommand("dragon")).setExecutor(new deez());
-        Objects.requireNonNull(getCommand("bruhconfig")).setExecutor(new ConfigUI());
-        Objects.requireNonNull(getCommand("syeet")).setExecutor(new superyeet());
-        Objects.requireNonNull(getCommand("ratio")).setExecutor(new ratio());
+        getServer().getPluginManager().registerEvents(new mpbEvent(), this);
+        getServer().getPluginManager().registerEvents(new mpbiEvent(), this);
+        getServer().getPluginManager().registerEvents(new adminactEvent(), this);
 
-        Logger logger = this.getLogger();
+        Objects.requireNonNull(getCommand("mpb")).setExecutor(new mpbUI());
+        getCommand("mpb").setPermissionMessage(ChatColor.translateAlternateColorCodes('&', prefix + " " + noperm));
+        Objects.requireNonNull(getCommand("mpbinfo")).setExecutor(new mpbiUI());
+        getCommand("mpbinfo").setPermissionMessage(ChatColor.translateAlternateColorCodes('&', prefix + " " + noperm));
+    }
 
-        new UpdateChecker(this, 85804).getVersion(version -> {
-            if (!this.getDescription().getVersion().equalsIgnoreCase(version)) {
-                Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Bruh" + ChatColor.GRAY + "" + ChatColor.BOLD + "CMD >> " + ChatColor.GOLD + "Update " + ChatColor.BOLD + "" + version + ChatColor.GRAY + " is available.");
-            }
-        });
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
     }
 }
