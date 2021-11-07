90133d3af5605c1b5206d48658a26835f8043f0f
icynnac <68450404+icynnac@users.noreply.github.com>
2021-11-07 09:55:02 -0500

    AdminactUI finally uh works.

---
+++ src/main/java/me/icynnac/multiplayerbasics/Events/adminactEvent.java
@@ -0,0 +1,45 @@
+package me.icynnac.multiplayerbasics.Events;
+
+import me.icynnac.multiplayerbasics.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.scheduler.BukkitScheduler;
+
+public class adminactEvent implements Listener {
+    @EventHandler
+    public void adminact(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.RED + "MultiplayerBasics Admin")) {
+            e.setCancelled(true);
+            switch (e.getCurrentItem().getType()) {
+                case WOOL:
+                    if (e.getCurrentItem().getDurability() == 14) {
+                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.translateAlternateColorCodes('&', Main.prefix + " &cShutting down...")));
+                        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+                        scheduler.scheduleSyncDelayedTask(Main.instance, () -> {
+                            for (Player all : Bukkit.getServer().getOnlinePlayers()) {
+                                all.kickPlayer(ChatColor.translateAlternateColorCodes('&', Main.kicksd1 + "\n" + Main.kicksd2 + "\n" + Main.kicksd3 + "\n" + Main.kicksd4));
+                            }
+                            Bukkit.getServer().shutdown();
+                        },20*2);
+                    }
+                    if (e.getCurrentItem().getDurability() == 5) {
+                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.prefix + " &aReloading..."));
+                        Bukkit.reload();
+                    }
+                    break;
+                case PAPER:
+                    p.closeInventory();
+                    p.performCommand("mpb");
+                    break;
+                case BARRIER:
+                    p.closeInventory();
+                    break;
+            }
+        }
+    }
+}
