13691e4b3333f28ccc9262fdde8e250542a70774
icynnac <68450404+icynnac@users.noreply.github.com>
2021-11-07 08:47:08 -0500

    hope this isnt broken!

--- src/main/java/me/icynnac/multiplayerbasics/Events/mpbEvent.java
+++ src/main/java/me/icynnac/multiplayerbasics/Events/mpbEvent.java
@@ -1,4 +1,29 @@
 package me.icynnac.multiplayerbasics.Events;
 
-public class mpbEvent {
+import me.icynnac.multiplayerbasics.GUIs.adminactUI;
+import me.icynnac.multiplayerbasics.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class mpbEvent implements Listener {
+    @EventHandler
+    public void mpb(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+        adminactUI adminact = new adminactUI();
+        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.AQUA + "MultiplayerBasics Panel")) {
+            switch (e.getCurrentItem().getType()) {
+                case WOOL:
+                    break;
+                case DIAMOND_SWORD:
+                    p.closeInventory();
+                    p.openInventory(adminact.getAdminActInventory());
+                    e.setCancelled(true);
+                    break;
+            }
+        }
+    }
 }
